1b868e77bb5cd2dfd9588651e8256918acf8aa28
risheek <vrisheek19@gmail.com>
2026-01-03 11:39:15 +0530

    Invalid command print

--- app/main.go
+++ app/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 var _ = fmt.Print;
 
 func main(){
 	fmt.Print("$ ")
+	reader := bufio.NewReader(os.Stdin)
+	text,error := reader.ReadString('\n')
+
+	if error!=nil {
+		fmt.Print("Error during printing")
+	}
+
+	//text = strings.TrimSpace(text);
+
+	fmt.Println(text[:len(text)-1] + ": command not found")
 }
