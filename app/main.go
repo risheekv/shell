0da00888657df93854d418e8901a80b3adeb7e44
risheek <vrisheek19@gmail.com>
2026-01-03 14:46:18 +0530

    type implementation

--- app/main.go
+++ app/main.go
@@ -16,7 +16,15 @@ func main(){
 		if error != nil {
 			fmt.Println("Error reported")
 		}
-		if command == "exit\n" {
+
+		if strings.HasPrefix(command,"type ") {
+			typeCommand := strings.TrimSpace(command[5:])
+			if(typeCommand == "echo" || typeCommand == "exit" || typeCommand == "type") {
+				fmt.Println(typeCommand + " is a shell builtin")
+			} else{
+				fmt.Println(typeCommand + ": not found")
+			}
+		} else if command == "exit\n" {
 			break
 		} else if strings.HasPrefix(command,"echo ") {
 			command := strings.TrimPrefix(command,"echo ")
